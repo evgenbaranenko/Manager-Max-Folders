01f0a296a596e3f7fcb57817bfc339e2aa6168c4
evgenbaranenko <evgenbaranenko@gmail.com>
2026-02-15 20:05:35 +0200

    eror commit - 02

--- Folders_Max_WinForm/obj/Release/net10.0-windows/Folders_Max_WinForm.AssemblyInfo.cs
+++ Folders_Max_WinForm/obj/Release/net10.0-windows/Folders_Max_WinForm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cbf95e06ee5b001c93d58d56c9e2f530407115a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86751153e1a14c3f6f510d88a359ab1c824f62e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
