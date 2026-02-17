24feb6afacc05820288846460923562b66128b04
evgenbaranenko <evgenbaranenko@gmail.com>
2026-02-17 16:23:20 +0200

    will go to main

--- Folders_Max_WinForm/obj/Release/net10.0-windows/Folders_Max_WinForm.AssemblyInfo.cs
+++ Folders_Max_WinForm/obj/Release/net10.0-windows/Folders_Max_WinForm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23911fffffd02eba28bd21fa5c2ba2168df73f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc41426e109a86c64781b0895b089997300a119f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
