82367d45ca223bc523d42117cd85ad4f17b2dcfd
evgenbaranenko <evgenbaranenko@gmail.com>
2026-02-22 15:16:08 +0200

    commit

--- Folders_Max_WinForm/obj/Debug/net10.0-windows/Folders_Max_WinForm.AssemblyInfo.cs
+++ Folders_Max_WinForm/obj/Debug/net10.0-windows/Folders_Max_WinForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+123249b4635bcdc2a0c653e3c8e9608ab425152f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc64483c89f3f9bc3d01921923f35b9f25fed19e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Folders_Max_WinForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
